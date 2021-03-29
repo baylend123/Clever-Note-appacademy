e9ba40fe710a34e59465286905a20064a4cccaac
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-03-28 19:37:03 -0500

    test

---
+++ backend/routes/api/index.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+router.post('/test', function (req, res) {
+    res.json({requestBody: req.body});
+});
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+module.exports = router;
