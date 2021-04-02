9edb18bf662c70feb301becc4b67c7466346fa26
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-04-02 10:00:03 -0500

    “pre heroku push”

--- frontend/src/components/WriteNote/WriteNote.js
+++ frontend/src/components/WriteNote/WriteNote.js
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import parse from "html-react-parser";
+
+import { saveNotes } from "../../store/notes";
 
 import "./WriteNote.css";
+
 const WriteNote = ({ note }) => {
-  const { noteId } = useParams();
+  const dispatch = useDispatch();
+  const { noteId, id } = useParams();
+  const noteBookId = parseInt(id, 10);
+
   let focusNote = "";
   if (note?.length) {
     focusNote = note.find((note) => note.id.toString() === noteId);
   }
+
   const [text, setText] = useState(focusNote.body);
-  console.log(focusNote);
+
+  const handleSave = () => {
+    console.log(noteId);
+    dispatch(saveNotes(text, noteBookId, noteId));
+  };
+
   return (
     <div className="note-container">
+      <button onClick={handleSave}>save</button>
       <CKEditor
         editor={ClassicEditor}
         data={text}
--- frontend/src/store/notes.js
+++ frontend/src/store/notes.js
@@ -1,40 +1,63 @@
-import { csrfFetch } from './csrf';
+import { csrfFetch } from "./csrf";
 
-const notesLoader = 'notes/load'
+const notesLoader = "notes/load";
+const noteSaver = "notes/save";
 
 const loadNotes = (notes) => {
+  return {
+    type: notesLoader,
+    payload: notes,
+  };
+};
+const saveNote = (note) => {
+  return {
+    type: noteSaver,
+    payload: note,
+  };
+};
 
-    return {
-        type: notesLoader,
-        payload: notes
-    }
+export const saveNotes = (note, noteBookId, noteId) => async (dispatch) => {
+  const body = { note, noteBookId, noteId };
+  console.log(body);
+  const result = await csrfFetch("/api/notes/save", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (result.status === 200) {
+    const savedNote = result.json();
+    console.log(savedNote);
+    dispatch(saveNote(savedNote));
+  }
+};
 
-}
+export const getNotes = (noteBookId) => async (dispatch) => {
+  const result = await csrfFetch(`/api/notes/${noteBookId}`);
+  if (result.status === 200) {
+    const notes = await result.json();
 
-
-export const getNotes = (noteBookId) => async dispatch => {
-    const result = await csrfFetch(`/api/notes/${noteBookId}`)
-    if (result.status === 200) {
-        const notes = await result.json()
-
-        dispatch(loadNotes(notes))
-    }
-
-}
+    dispatch(loadNotes(notes));
+  }
+};
 const initialState = {
-    notes: null
-}
+  notes: null,
+};
 
 const notesReducer = (state = initialState, action) => {
-    let newState;
-    switch (action.type) {
-        case notesLoader:
-            newState = Object.assign({}, state);
-            newState.notes = action.payload
-            return newState
-        default:
-            return state
-    }
-
-}
-export default notesReducer
+  let newState;
+  switch (action.type) {
+    case notesLoader:
+      newState = Object.assign({}, state);
+      newState.notes = action.payload;
+      return newState;
+    case noteSaver:
+      newState = Object.assign({}, state);
+      newState.notes.push(action.payload);
+      return newState;
+    default:
+      return state;
+  }
+};
+export default notesReducer;
