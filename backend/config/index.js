dc27ef7e064b2cedd84752ad9d3bfa15d7f5340c
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-04-05 10:22:21 -0500

    “”

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    enviroment: process.env.NODE_ENV || 'development',
+    environment: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 5000,
     db: {
         username: process.env.DB_USERNAME,
