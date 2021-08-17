d0696b2755d6bf1ddfc01ce90de173c6d6db3df2
baylen doss <baylendoss@baylens-MacBook-Air.local>
2021-08-17 13:57:14 -0500

    test3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
     dispatch(sessionActions.login({ credential: 'Demo-lition', password: 'password' }))
     history.push(`/`)
   }
-  demoUser()
+  // demoUser()
   return (
     <div className='main-container'>
           <Route path='/login' exact={true}>
