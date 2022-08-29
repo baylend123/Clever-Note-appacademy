a8edfa8fec314d3883a24a75397a9db1be47665e
baylen doss <baylendoss12@gmail.com>
2022-08-29 12:02:03 -0500

    styling

--- frontend/src/components/WriteNote/WriteNote.js
+++ frontend/src/components/WriteNote/WriteNote.js
@@ -1,24 +1,41 @@
 import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Editor, EditorState, convertToRaw, convertFromHTML, convertFromRaw } from 'draft-js';
+import {RichUtils, Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 import { saveNotes, newNote } from '../../store/notes';
 import './WriteNote.css';
 
 const WriteNote = () => {
+  const dispatch = useDispatch();
   const { noteId } = useParams();
   const { notebookId } = useParams();
+  
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  
+  
   const notes = useSelector(state => state?.notes)
   let focusNote = notes?.find((note) => note?.id.toString() === noteId);
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-  const dispatch = useDispatch();
+  
+  
   useEffect(() => {
     if (focusNote) {
       setEditorState(EditorState.createWithContent(convertFromRaw(JSON.parse(focusNote.body))))
     }
   }, [focusNote])
+
+  useEffect(() => {console.log('state change')},[editorState])
+
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if(newState){
+      setEditorState(newState)
+    }
+  }
+
+
   const handleSave = async () => {
     if (noteId === 'new' && notebookId) {
 
@@ -45,9 +62,15 @@ const WriteNote = () => {
         Save Your Note
       </button>
       <div className='text-editor'>
+      <button
+      onClick={() => setEditorState((prev) => RichUtils.toggleInlineStyle(prev, 'BOLD'))}
+      >
+        Test Bold
+      </button>
         <Editor
           editorState={editorState}
           onChange={setEditorState}
+          handleKeyCommand={handleKeyCommand}
         />
       </div>
     </div>
